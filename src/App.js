48222e0973997490866ff00df9e0f969966cf5e4
Nhan Dinh <huunhancit@gmail.com>
2021-10-10 12:05:40 +0700

    Config husky to pre-commit format code

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>Tracking App</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
