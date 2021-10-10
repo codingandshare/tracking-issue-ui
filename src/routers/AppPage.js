cf0b2c391c52446380ce4e081aa717b24de3bd5d
Nhan Dinh <huunhancit@gmail.com>
2021-10-10 12:39:49 +0700

    config router for web

---
+++ src/routers/AppPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AppPage = () => {
+  return <div>home</div>
+}
+
+export default AppPage
---
+++ src/routers/AuthenPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AuthenPage = () => {
+  return <div>AuthenPage</div>
+}
+
+export default AuthenPage
